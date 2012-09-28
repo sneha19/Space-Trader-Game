32d214a66382881ac895686886640fadc4e93f1a
haysam121@gmail.com <haysam121@gmail.com@b47cf0e7-3563-6f0a-96fe-3156760c1070>
2012-09-28 20:11:00 +0000

    person's file. Everyone do one!

---
+++ M4/src/edu/gatech/oad/antlab/person/Person1.java
@@ -0,0 +1,58 @@
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 1
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person1 {
+  /** Holds the persons real name */
+  private String name;
+   /**
+  * The constructor, takes in the persons
+  * name
+  * @param pname the person's real name
+  */
+  public Person1(String pname) {
+    name = pname;
+  }
+   /**
+  * This method should take the string
+  * input and return its characters rotated
+  * 2 positions.
+  * given "gtg123b" it should return
+  * "g123bgt".
+  *
+  * @param input the string to be modified
+  * @return the modified string
+  */
+ private String calc(String input) {
+	  String temp = "";
+	  if(input == null) throw new NullPointerException("not a string!");
+	  String lastTwo = input.substring(0, 2);
+	  int index = 2;
+	  while(index < input.length())
+	  {
+		  temp = temp + input.substring(index, index+1);
+		  index++;
+	  }
+	  temp = temp + lastTwo;
+	  return temp;
+	 }
+ 
+ /**
+  * Return a string rep of this object
+  * that varies with an input string
+  *
+  * @param input the varying string
+  * @return the string representing the 
+  *         object
+  */
+ public String toString(String input) {
+   return name + calc(input);
+ }
+
+}
