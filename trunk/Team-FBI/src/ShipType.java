487a3996d5bf6ab88491164beb687de190d8f7f8
sneha.ganesh2856 <sneha.ganesh2856@b47cf0e7-3563-6f0a-96fe-3156760c1070>
2012-10-05 19:52:12 +0000

    ShipType update

--- trunk/Team-FBI/src/ShipType.java
+++ trunk/Team-FBI/src/ShipType.java
@@ -1,10 +1,20 @@
 public enum ShipType {
-	private Object _fighter;
-	private Object _hauler;
-	private Object _bullDog;
-	private Object _carrier;
-	private Object _battleShip;
-	private Object _yelowJacket;
-	private Object _destroyer;
-	private Object _heat;
+	
+	private Ship fighter;
+	private Ship hauler;
+	private Ship bullDog;
+	private Ship carrier;
+	private Ship battleShip;
+	private Ship yelowJacket;
+	private Ship destroyer;
+	private Ship heat;
+	
+	public setEverything(){
+		ShipType.fighter.setHull(100);
+		ShipType.fighter.setCost(100);
+		ShipType.fighter.setCargoCapacity(20);
+		Shiptype.fighter.setFuelCapacity(100);
+	}
+	 
+	
 }
