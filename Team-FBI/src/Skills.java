b45c3c44d1662bc29386d3a317434f760871e014
haysam121 <haysam121@b47cf0e7-3563-6f0a-96fe-3156760c1070>
2012-10-05 19:47:16 +0000

    skills and skills gui up

--- Team-FBI/src/Skills.java
+++ Team-FBI/src/Skills.java
@@ -1,14 +1,60 @@
 import java.util.Vector;
+import javax.swing.JPanel;
 
-public class Skills {
-	private int _pilot;
-	private int _fighter;
-	private int _trader;
-	private int _engineer;
-	private int _points;
-	public Vector<Player_> _unnamed_Player__ = new Vector<Player_>();
-
-	public void setSkill() {
-		throw new UnsupportedOperationException();
-	}
+public class Skills{
+ private int pilotPoints;
+ private int fighterPoints;
+ private int traderPoints;
+ private int engineerPoints;
+ private int playerPoints = 16;
+ Player player;
+ public Skills(Player currPlayer)
+ {
+  player = currPlayer;
+ }
+ 
+ //public Vector<Player_> unnamed_Player = new Vector<Player_>();
+ public void setPilotPoints(int points)
+ {
+  pilotPoints += points;
+ }
+ public void setFighterPoints(int points)
+ {
+  fighterPoints += points;
+ }
+ public void setTraderPoints(int points)
+ {
+  traderPoints += points;
+ }
+ public void setEngineerPoints(int points)
+ {
+  engineerPoints += points;
+ }
+ public int getPilotPoints()
+ {
+  return pilotPoints;
+ }
+ public int getFighterPoints()
+ {
+  return fighterPoints;
+ }
+ public int getTraderPoints()
+ {
+  return traderPoints;
+ }
+ public int getEngineerPoints()
+ {
+  return engineerPoints;
+ }
+ public void setCurrPoints(int points)
+ {
+  playerPoints = playerPoints + points;
+ }
+ public int getCurrPoints()
+ {
+  return playerPoints;
+ }
+ public void setSkill() {
+  throw new UnsupportedOperationException();
+ }
 }
