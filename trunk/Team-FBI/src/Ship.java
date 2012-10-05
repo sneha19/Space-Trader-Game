74e296c9c92dbeecc6eddea765e96a1a12ed453a
sneha.ganesh2856 <sneha.ganesh2856@b47cf0e7-3563-6f0a-96fe-3156760c1070>
2012-10-05 19:52:36 +0000

    Ship getters and setters updated

--- trunk/Team-FBI/src/Ship.java
+++ trunk/Team-FBI/src/Ship.java
@@ -1,11 +1,45 @@
 public abstract class Ship {
-	private int _hull;
-	private int _cargoCapacity;
-	private int _fuelCapacity;
-	private int _cost;
-	public Player_ _has_a;
+	private int hull;
+	private int cargoCapacity;
+	private int fuelCapacity;
+	private int cost;
+	public Player player;
 
 	public void setFuelAfterMove() {
 		throw new UnsupportedOperationException();
 	}
+	
+	public void setHull(int hull){
+		this.hull = hull;
+	}
+	
+	public int gethull(){
+		return hull;
+	}
+	
+	public void setCargoCapacity(int cargo){
+		cargoCapacity = cargo;
+	}
+	
+	public int getCargoCapacity(){
+		return(cargoCapacity);
+	}
+	
+	public void setFuelCapacity(int fuel){
+		fuelCapacity = fuel;
+	}
+	
+	public int getFuelCapacity(){
+		return(fuelCapacity);
+	}
+	
+	public void setCost(int cost){
+		this.cost = cot;
+	}
+	
+	public int getCost(){
+		return cost;
+	}
+	
+	
 }
