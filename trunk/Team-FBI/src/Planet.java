256aeff58c07104db8cf39480fa99073769ab777
samarth.paliwal <samarth.paliwal@b47cf0e7-3563-6f0a-96fe-3156760c1070>
2012-10-09 03:27:17 +0000

    Updated the planet using the enumeration.

--- trunk/Team-FBI/src/Planet.java
+++ trunk/Team-FBI/src/Planet.java
@@ -1,6 +1,61 @@
 public class Planet {
-	private String _planetName;
-	public Player_ _unnamed_Player__;
-	public Goverments _unnamed_Goverments_;
-	public Stats _unnamed_Stats_;
+	private String planetName;
+	//public Player player;
+	public Goverments goverments;
+	public Point location;
+	
+	public enum Planets
+	{
+		 utopia("Utopia",democracy, (3,5)),
+		 sparta("Sparta",anarchy,(200,155)),
+		 erebus("Erebus",corporateState,(35,185)),
+		 pheonix("Pheonix",morarchy,(100,100)),
+		 dis("Dis",militaryState,(250,60)),
+		 fortuna("Fortuna",democracy,(35,60)),
+		 hydra("Hydra",monarchy,(115,90)),
+		 pax("Pax",anarchy,(190,140)),
+		 sol("Sol",democracy,(50,50)),
+		 asgard("Asgard",monrachy,(175,50));
+	
+	
+	public Planets(String planetName, Governments goverments,  Point location)
+	{	
+		this.planetName = planetName;
+		this.goverments = gov;
+		this.location = location;
+	}
+	
+	public String getPlanetName()
+	{
+		return planetName;
+	}
+	
+	public Goverments getGoverment()
+	{	return goverments;}
+	
+	public Point getLocation()
+	{	return location;}
+	
+	}
+	
+	public String setPlanetName(String planetName)
+	{
+		this.planetName = planetName;
+	}
+	
+	public Goverments setGoverment(Goverments goverments)
+	{	
+		this.goverments = goverments;	
+	}
+	
+	public Point setLocation(Point locaction)
+	{	
+		this.location = location;
+	}
+	
+	}
+	
+	public Planet()
+	{}
+	
 }
