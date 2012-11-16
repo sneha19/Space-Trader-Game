6032c2fec50ff2b677a77826570a8a5dcf926ca4
haysam121 <haysam121@b47cf0e7-3563-6f0a-96fe-3156760c1070>
2012-11-16 23:08:02 +0000

    skills interface and stardock audit

--- Team-FBI/src/SkillsInterface.java
+++ Team-FBI/src/SkillsInterface.java
@@ -1,17 +1,18 @@
+// $codepro.audit.disable fileComment, variableShouldBeFinal, lineLength, methodChainLength, numericLiterals
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+
 import javax.swing.JPanel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
-import javax.swing.JSpinner;
+
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
@@ -19,84 +20,145 @@ import java.awt.Color;
 /**
  * GUI for skills
  * @author Haytham Abutair
- *
+ * @version 1
  */
 public class SkillsInterface extends JPanel {
-	Player currPlayer;
-	Skills currSkills;
-	int currentPoints;
-	JLabel name111;
-	
-	JLabel lblEngineering = new JLabel("Engineering: ");
-	
-	JLabel lblSkillsMenu =new JLabel("SKILLS MENU");
-	
-	JLabel lblFighter = new JLabel("Fighter:");
-	
-	JLabel lblPilot = new JLabel("Pilot:");
-	
-	JLabel lblTrader = new JLabel("Trader: ");
-	
-	JLabel lblPointsRemaning = new JLabel("Points remaning: ");
-	
-	JLabel lblSkillsPointsRemaning;
-	
-    JLabel lblEngineeringPoints; 
-	
-	JLabel lblFighterPoints; 
-	
-	JLabel lblPilotPoints; 
-	
-	JLabel lblTraderPoints;
-	
-	JButton btnNext = new JButton("Next");
-	
-	JButton btnEngineering = new JButton("Increase");
-	JButton btnFighter = new JButton("Increase");
-	
-	JButton btnPilot = new JButton("Increase");
-	
-	JButton btnTrader = new JButton("Increase");
+	/**
+	 * curr player
+	 */
+	private Player currPlayer;
+
+	/**
+	 * curr skills
+	 */
+	private Skills currSkills;
+
+	/**
+	 * curr points
+	 */
+	private int currentPoints;
+
+	/**
+	 * engineering
+	 */
+	private JLabel lblEngineering = new JLabel("Engineering: ");
+
+	/**
+	 * skills
+	 */
+	private JLabel lblSkillsMenu =new JLabel("SKILLS MENU");
+
+	/**
+	 * fighter
+	 */
+	private JLabel lblFighter = new JLabel("Fighter:");
+
+	/**
+	 * pilot
+	 */
+	private JLabel lblPilot = new JLabel("Pilot:");
+
+	/**
+	 * trader
+	 */
+	private JLabel lblTrader = new JLabel("Trader: ");
+
+	/**
+	 * points remaning
+	 */
+	private JLabel lblPointsRemaning = new JLabel("Points remaning: ");
+
+	/**
+	 * skills points
+	 */
+	private JLabel lblSkillsPointsRemaning;
+
+	/**
+	 * engineering points
+	 */
+	private JLabel lblEngineeringPoints; 
+
+	/**
+	 * fighter butotn
+	 */
+	private JLabel lblFighterPoints; 
+
+	/**
+	 * pilot button
+	 */
+	private JLabel lblPilotPoints; 
+
+	/**
+	 * trader button
+	 */
+	private JLabel lblTraderPoints;
+
+	/**
+	 * next button
+	 */
+	private JButton btnNext = new JButton("Next");
+
+	/**
+	 * engineering button
+	 */
+	private JButton btnEngineering = new JButton("Increase");
+
+	/**
+	 * figher button
+	 */
+	private JButton btnFighter = new JButton("Increase");
+
+	/**
+	 * pilot button
+	 */
+	private JButton btnPilot = new JButton("Increase");
+
+	/**
+	 * trade button
+	 */
+	private JButton btnTrader = new JButton("Increase");
+
 	/**
 	 * Create the panel.
+	 * @param skills
 	 */
-	public SkillsInterface(Skills skills) {
+	public SkillsInterface(Skills skills) { // $codepro.audit.disable
 		btnNext.setVisible(false);
 		currSkills=skills;
 		currPlayer=skills.getPlayer();
 		currentPoints = currSkills.getCurrPoints();
-		
-		
-		lblSkillsPointsRemaning = new JLabel(Integer.toString(currSkills.getCurrPoints()));
+
+		lblSkillsPointsRemaning = new JLabel
+				(Integer.toString(currSkills.getCurrPoints()));
 		lblSkillsPointsRemaning.setForeground(new Color(255, 255, 255));
 		
-	    lblEngineeringPoints = new JLabel(Integer.toString(currSkills.getEngineerPoints()));
+	    lblEngineeringPoints = new JLabel(Integer.toString
+	    		(currSkills.getEngineerPoints()));
 	    lblEngineeringPoints.setForeground(new Color(255, 255, 255));
 		
-		lblFighterPoints = new JLabel(Integer.toString(currSkills.getFighterPoints()));
+		lblFighterPoints = new JLabel(Integer.toString
+				(currSkills.getFighterPoints()));
 		lblFighterPoints.setForeground(new Color(255, 255, 255));
 		
-		lblPilotPoints = new JLabel(Integer.toString(currSkills.getPilotPoints()));
+		lblPilotPoints = new JLabel(Integer.toString
+				(currSkills.getPilotPoints()));
 		lblPilotPoints.setForeground(new Color(255, 255, 255));
 		
-		lblTraderPoints = new JLabel(Integer.toString(currSkills.getTraderPoints()));
+		lblTraderPoints = new JLabel(Integer.toString
+				(currSkills.getTraderPoints()));
 		lblTraderPoints.setForeground(new Color(255, 255, 255));
-		
-		
-		
 
 		btnFighter.addActionListener(new ActionListener() {
 			/**
 			 * actionPerformed for the fighter button
+			 * @param e action
 			 */
 			public void actionPerformed(ActionEvent e) {
-				if(currSkills.getCurrPoints() >= 1)
-				{
+				if(currSkills.getCurrPoints() >= 1){
 					currSkills.setFighterPoints(1);
 					currSkills.setCurrPoints(-1);
 					currPlayer.setAvailableSkillPoints(-1);
-					if(currSkills.getCurrPoints() == 0)
-					{
+					if(currSkills.getCurrPoints() == 0){
 						btnNext.setVisible(true);
 					}
 					
@@ -108,16 +170,15 @@ public class SkillsInterface extends JPanel {
 		btnEngineering.addActionListener(new ActionListener() {
 			/**
 			 * actionPerformed for the Engineer button
+			 * @param e action
 			 */
 			public void actionPerformed(ActionEvent e) {
-				if(currSkills.getCurrPoints() >= 1)
-				{
+				if(currSkills.getCurrPoints() >= 1){
 					currSkills.setEngineerPoints(1);
 					currSkills.setCurrPoints(-1);
 					currPlayer.setAvailableSkillPoints(-1);
 					
-					if(currSkills.getCurrPoints() == 0)
-					{
+					if(currSkills.getCurrPoints() == 0){
 						btnNext.setVisible(true);
 					}
 					
@@ -130,17 +191,16 @@ public class SkillsInterface extends JPanel {
 		btnPilot.addActionListener(new ActionListener() {
 			/**
 			 * actionPerformed for the Pilot button
+			 *  @param e action
 			 */
 			public void actionPerformed(ActionEvent e) {
-				if(currSkills.getCurrPoints() >= 1)
-				{
+				if(currSkills.getCurrPoints() >= 1){
 					currSkills.setPilotPoints(1);
 					currSkills.setCurrPoints(-1);
 					currPlayer.setAvailableSkillPoints(-1);
 
 					btnNext.setVisible(false);
-					if(currSkills.getCurrPoints() == 0)
-					{
+					if(currSkills.getCurrPoints() == 0){
 						btnNext.setVisible(true);
 					}
 					
@@ -152,26 +212,26 @@ public class SkillsInterface extends JPanel {
 		btnTrader.addActionListener(new ActionListener() {
 			/**
 			 * actionPerformed for the Trader button
+			 * @param e action
 			 */
 			public void actionPerformed(ActionEvent e) {
-				if(currSkills.getCurrPoints() >= 1)
-				{
+				if(currSkills.getCurrPoints() >= 1){
 					currSkills.setTraderPoints(1);
 					currSkills.setCurrPoints(-1);
 					currPlayer.setAvailableSkillPoints(-1);
 					
-					if(currSkills.getCurrPoints() == 0)
-					{
+					if(currSkills.getCurrPoints() == 0){
 						btnNext.setVisible(true);
 					}
 					
 				}
-				lblSkillsPointsRemaning.setText(Integer.toString(currSkills.getCurrPoints()));
-				lblTraderPoints.setText(Integer.toString(currSkills.getTraderPoints()));
+				lblSkillsPointsRemaning.setText
+				(Integer.toString(currSkills.getCurrPoints()));
+				lblTraderPoints.setText(Integer.toString
+						(currSkills.getTraderPoints()));
 			}
 		});
-		
-	
+
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -252,18 +312,22 @@ public class SkillsInterface extends JPanel {
 		);
 		setLayout(groupLayout);
 	}
+
 	/**
 	 * actionPerformed for the next button
+	 * @param al action
 	 */
 	public void setbtnNextListener(ActionListener al){
 		btnNext.addActionListener(al);
 	}
+
 	/**
 	 * disables the next button
 	 */
 	public void disablebtnNext(){
 		btnNext.setVisible(false);
 	}
+
 	/**
 	 * draw background
 	 * @param g a Graphics instance
@@ -271,6 +335,6 @@ public class SkillsInterface extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Image bg = new ImageIcon("img/bricks.jpg").getImage();
-		g.drawImage(bg, 0, 0,getWidth(),getHeight(), this);
+		g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
 	}
 }
--- Team-FBI/src/StarDock.java
+++ Team-FBI/src/StarDock.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+import java.awt.Color; // $codepro.audit.disable fileComment, lossOfPrecisionInCast, numericLiterals, com.instantiations.assist.eclipse.analysis.unusedReturnValue, variableShouldBeFinal, emptyMethod
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -21,112 +21,165 @@ import javax.swing.event.ChangeListener;
 
 import net.miginfocom.swing.MigLayout;
 
-import org.omg.CORBA.TRANSIENT;
+
 
 /**
  * stardock gui side to allow users to interact with game
  * @author Haytham Abutair
+ * @version 1
  *
  */
 public class StarDock extends JPanel  implements Serializable{
-	public Zone _unnamed_Zone_;
+	/**
+	 * zone
+	 */
+	public Zone _unnamed_Zone_; // $codepro.audit.disable instanceFieldNamingConvention
 
-	private int fuel = 25;
+	/**
+	 * fuel
+	 */
+	private int fuel = 25; // $codepro.audit.disable numericLiterals
 
-	private int hull = 10;
+	/**
+	 * hull
+	 */
+	private int hull = 10; // $codepro.audit.disable numericLiterals
 
+/**
+ * CURRPLayer
+ */
 	private Player currPlayer;
 
-	JButton btnContinue;
+/**
+ * continue
+ */
+	private JButton btnContinue;
 
-	JComboBox comboBox;
+/**
+ * combobox
+ */
+	private JComboBox comboBox;
 
-	JLabel currentHullLbl = new JLabel("");
+/**
+ * hull lbl
+ */
+	private JLabel currentHullLbl = new JLabel("");
 
-	JLabel currShiplbl = new JLabel("");
+/**
+ * ship lbl
+ */
+	private JLabel currShiplbl = new JLabel("");
 
-	JLabel cashLabel = new JLabel("");
+/**
+ * cash lbl
+ */
+	private JLabel cashLabel = new JLabel("");
 
-	JLabel currFuelLbl = new JLabel("");
+/**
+ * fuel lbl
+ */
+	private JLabel currFuelLbl = new JLabel("");
 
-	JSlider hullSlider;
+/**
+ * hull slider
+ */
+	private JSlider hullSlider;
 
-	JSlider fuelSlider;
+/**
+ * fuel slider
+ */
+	private JSlider fuelSlider;
 
-	JLabel maxFuelLbl = new JLabel("");
+/**
+ * maxfuel lbl
+ */
+	private JLabel maxFuelLbl = new JLabel("");
 
-	JLabel maxHullLbl = new JLabel("");
+/**
+ * max hull lbl
+ */
+	private JLabel maxHullLbl = new JLabel("");
 
-	transient Image bg;
-
-	int i = 0;
+/**
+ * image 
+ */
+	private transient Image bg;
 
 	/**
 	 * Create the panel.
+	 * @param p player
 	 */
-	public StarDock(Player p) {
+	public StarDock(Player p) { // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.audit.rule.effectivejava.constructorsOnlyInvokeFinalMethods
 		currPlayer = p;
-		setBackground(new Color(51, 102, 102));
+		setBackground(new Color(51, 102, 102)); // $codepro.audit.disable numericLiterals
 		setForeground(UIManager.getColor("Desktop.background"));
-		setLayout(new MigLayout("", "[][][grow][][][][][][][][][][][][][][][][][grow]", "[][][][][][][][][][][][][][][][grow][]"));
-		JLabel lblBuyShip = new JLabel("Buy Ship:");
-		lblBuyShip.setForeground(new Color(255, 255, 255));
+		setLayout(new MigLayout("", "[][][grow][][][][][][][][][][][][][][][][][grow]", 
+				"[][][][][][][][][][][][][][][][grow][]"));
+		JLabel lblBuyShip = new JLabel("Buy Ship:"); // $codepro.audit.disable variableShouldBeFinal
+		lblBuyShip.setForeground(new Color(255, 255, 255)); // $codepro.audit.disable numericLiterals
 		add(lblBuyShip, "cell 1 1");
 
 		comboBox = new JComboBox();
 		//		fighter("Fighter",100,20,100,100),hauler("Hauler",120,100,80,700),bullDog("Bulldog",200,50,50,300),carrier("Carrier",60,150,80,350),yellowJacket("Yellow Jacket",140,30,150,300),destroyer("Destroyer",500,50,100,700),heat("Heat",100,100,100,400);
 
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Fighter - $1550", "Hauler - $2000", "Bulldog - $1000", "Carrier - $1100", "YellowJacket - $2000", "Destroyer - $1450", "Heat - $3000"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Fighter - $1550",
+				"Hauler - $2000", "Bulldog - $1000", "Carrier - $1100", 
+				"YellowJacket - $2000", "Destroyer - $1450", "Heat - $3000"}));
 
 		add(comboBox, "cell 9 1,growx");
 
-		JButton btnGo = new JButton("Buy Ship!");
+		JButton btnGo = new JButton("Buy Ship!"); // $codepro.audit.disable variableShouldBeFinal
 		btnGo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String output = (String)comboBox.getSelectedItem();
+			public void actionPerformed(ActionEvent arg0) { // $codepro.audit.disable cyclomaticComplexity
+				String output = (String) comboBox.getSelectedItem(); // $codepro.audit.disable variableShouldBeFinal
 				int i = 0;
 				String finalName = "";
-				while( output.charAt(i) != ' ')
-				{
-					finalName += output.charAt(i);
+				while( output.charAt(i) != ' '){
+					finalName += output.charAt(i); // $codepro.audit.disable stringConcatenationInLoop
 					i++;
 				}
-				String fighter = "Fighter";
-				String hauler = "Hauler";
-				String bulldog = "Bulldog";
-				String carrier = "Carrier";
-				String yellowJacket = "YellowJacket";
-				String destroyer = "Destroyer";
-				String heat = "Heat";
-				Ship tempShip;
-				if(finalName.equals(heat) && currPlayer.getCash() >= 1050 && !currPlayer.getShip().getName().equals("Heat")){
-					currPlayer.setCash(-1050);
-					currPlayer.setShip(new Ship(ShipType.heat));
+				String fighter = "Fighter"; // $codepro.audit.disable variableShouldBeFinal
+				String hauler = "Hauler"; // $codepro.audit.disable variableShouldBeFinal
+				String bulldog = "Bulldog"; // $codepro.audit.disable variableShouldBeFinal
+				String carrier = "Carrier"; // $codepro.audit.disable variableShouldBeFinal
+				String yellowJacket = "YellowJacket"; // $codepro.audit.disable variableShouldBeFinal
+				String destroyer = "Destroyer"; // $codepro.audit.disable variableShouldBeFinal
+				String heat = "Heat"; // $codepro.audit.disable variableShouldBeFinal
+				if(finalName.equals(heat) && currPlayer.getCash() >= 1050 && // $codepro.audit.disable numericLiterals
+						!currPlayer.getShip().getName().equals("Heat")){
+					currPlayer.setCash(-1050); // $codepro.audit.disable numericLiterals, com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.setShip(new Ship(ShipType.heat)); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 				}
-				if(finalName.equals(destroyer) && currPlayer.getCash() >= 2450 && !currPlayer.getShip().getName().equals("Destroyer")){
-					currPlayer.setCash(-1450);
-					currPlayer.setShip(new Ship(ShipType.destroyer));
+				if(finalName.equals(destroyer) && currPlayer.getCash() >= 
+						2450 && !currPlayer.getShip().getName().equals("Destroyer")){ // $codepro.audit.disable numericLiterals
+					currPlayer.setCash(-1450); // $codepro.audit.disable numericLiterals, com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.setShip(new Ship(ShipType.destroyer)); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 				}
-				if(finalName.equals(yellowJacket) && currPlayer.getCash() >= 2000 && !currPlayer.getShip().getName().equals("Yellow Jacket")){
-					currPlayer.setCash(-2000);
-					currPlayer.setShip(new Ship(ShipType.yellowJacket));
+				if(finalName.equals(yellowJacket) && currPlayer.getCash() >= 
+						2000 && !currPlayer.getShip().getName().equals("Yellow Jacket")){ // $codepro.audit.disable numericLiterals
+					currPlayer.setCash(-2000); // $codepro.audit.disable numericLiterals, com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.setShip(new Ship(ShipType.yellowJacket)); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 				}
-				if(finalName.equals(carrier) && currPlayer.getCash() >= 1100 && !currPlayer.getShip().getName().equals("Carrier")){
-					currPlayer.setCash(-1100);
-					currPlayer.setShip(new Ship(ShipType.carrier));
+				if(finalName.equals(carrier) && currPlayer.getCash() >= 1100  // $codepro.audit.disable numericLiterals
+						&& !currPlayer.getShip().getName().equals("Carrier")){
+					currPlayer.setCash(-1100); // $codepro.audit.disable numericLiterals, com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.setShip(new Ship(ShipType.carrier)); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 				}
-				if(finalName.equals(fighter) && currPlayer.getCash() >= 1550 && !currPlayer.getShip().getName().equals("Fighter")){
-					currPlayer.setCash(-1550);
-					currPlayer.setShip(new Ship(ShipType.fighter));
+				if(finalName.equals(fighter) && currPlayer.getCash() >= 1550 // $codepro.audit.disable numericLiterals
+						&& !currPlayer.getShip().getName().equals("Fighter")){
+					currPlayer.setCash(-1550); // $codepro.audit.disable numericLiterals, com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.setShip(new Ship(ShipType.fighter)); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 				}
-				if(finalName.equals(hauler) && currPlayer.getCash() >= 2000 && !currPlayer.getShip().getName().equals("Hauler")){
-					currPlayer.setCash(-2000);
-					currPlayer.setShip(new Ship(ShipType.hauler));
+				if(finalName.equals(hauler) && currPlayer.getCash() >= 2000  // $codepro.audit.disable numericLiterals
+						&& !currPlayer.getShip().getName().equals("Hauler")){
+					currPlayer.setCash(-2000); // $codepro.audit.disable numericLiterals, com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.setShip(new Ship(ShipType.hauler)); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 				}
 
-				if(finalName.equals(bulldog) && currPlayer.getCash() >= 1000 && !currPlayer.getShip().getName().equals("Bulldog")){
-					currPlayer.setCash(-1000);
-					currPlayer.setShip(new Ship(ShipType.bullDog));
+				if(finalName.equals(bulldog) && currPlayer.getCash() >= 1000
+						&& !currPlayer.getShip().getName().equals("Bulldog")){
+					currPlayer.setCash(-1000); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.setShip(new Ship(ShipType.bullDog)); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 				}
 
 				updateLabels();
@@ -136,16 +189,16 @@ public class StarDock extends JPanel  implements Serializable{
 		});
 		add(btnGo, "cell 11 1");
 
-		JLabel lblRepairHull = new JLabel("Repair Hull: @ $10/e");
+		JLabel lblRepairHull = new JLabel("Repair Hull: @ $10/e"); // $codepro.audit.disable variableShouldBeFinal
 		lblRepairHull.setForeground(new Color(255, 255, 255));
 		add(lblRepairHull, "cell 1 2");
 		//hullSpinner.
 		hullSlider = new JSlider();
-		hullSlider.setMaximum((int) (currPlayer.getShip().getMaxHull() - currPlayer.getShip().getHull()));
+		hullSlider.setMaximum((int) (currPlayer.getShip().getMaxHull() -  // $codepro.audit.disable lossOfPrecisionInCast
+				currPlayer.getShip().getHull()));
 		hullSlider.setOrientation(SwingConstants.VERTICAL);
 		hullSlider.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent arg0) {
-				int calcPrice = currPlayer.getCash()/hull;
+			public void stateChanged(ChangeEvent arg0) { // $codepro.audit.disable emptyMethod
 				//		if(hullSlider.getValue() < calcPrice)
 				//		{
 				//			hullSlider.setValue(hullSlider.getValue() + 1);
@@ -163,13 +216,15 @@ public class StarDock extends JPanel  implements Serializable{
 		hullSlider.setToolTipText("");
 		add(hullSlider, "flowx,cell 9 2");
 
-		JButton btnRepair = new JButton("Repair!");
+		JButton btnRepair = new JButton("Repair!"); // $codepro.audit.disable variableShouldBeFinal
 		btnRepair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if(hullSlider.getValue() * hull <= currPlayer.getCash() && (hullSlider.getValue()+currPlayer.getShip().getHull() <= currPlayer.getShip().getMaxHull()))
-				{
-					currPlayer.setCash(-hullSlider.getValue() * hull);
-					currPlayer.getShip().setHull(currPlayer.getShip().getHull() + hullSlider.getValue());
+				if(hullSlider.getValue() * hull <= currPlayer.getCash()
+						&& (hullSlider.getValue() + currPlayer.getShip().getHull() 
+								<= currPlayer.getShip().getMaxHull())){
+					currPlayer.setCash(-hullSlider.getValue() * hull); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
+					currPlayer.getShip().setHull(currPlayer.getShip().getHull() 
+							+ hullSlider.getValue());
 					updateLabels();
 				}
 			}
@@ -188,15 +243,17 @@ public class StarDock extends JPanel  implements Serializable{
 		fuelSlider.setForeground(new Color(255, 255, 255));
 		fuelSlider.setMinimum(0);
 		fuelSlider.setMinorTickSpacing(0);
-		fuelSlider.setMaximum((int) (currPlayer.getShip().getFuelCapacity() - currPlayer.getShip().getCurrentFuel()));
+		fuelSlider.setMaximum((int) (currPlayer.getShip().getFuelCapacity() // $codepro.audit.disable lossOfPrecisionInCast
+				- currPlayer.getShip().getCurrentFuel()));
 		add(fuelSlider, "cell 9 3");
 
-		JButton btnPumpFuel = new JButton("Pump Fuel!");
+		JButton btnPumpFuel = new JButton("Pump Fuel!"); // $codepro.audit.disable variableShouldBeFinal
 		btnPumpFuel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if(fuelSlider.getValue() * fuel <= currPlayer.getCash() && (fuelSlider.getValue()+currPlayer.getShip().getCurrentFuel() <= currPlayer.getShip().getFuelCapacity()))
-				{
-					currPlayer.setCash(-fuelSlider.getValue() * fuel);
+				if(fuelSlider.getValue() * fuel <= currPlayer.getCash() &&
+						(fuelSlider.getValue() + currPlayer.getShip().getCurrentFuel() 
+						<= currPlayer.getShip().getFuelCapacity())){
+					currPlayer.setCash(-fuelSlider.getValue() * fuel); // $codepro.audit.disable com.instantiations.assist.eclipse.analysis.unusedReturnValue
 					currPlayer.getShip().updateCurrentFuel(fuelSlider.getValue());
 					updateLabels();
 				}
@@ -204,7 +261,7 @@ public class StarDock extends JPanel  implements Serializable{
 		});
 		add(btnPumpFuel, "cell 11 3");
 
-		JLabel lblCurrentAmount = new JLabel("Current cash:");
+		JLabel lblCurrentAmount = new JLabel("Current cash:"); // $codepro.audit.disable variableShouldBeFinal
 		lblCurrentAmount.setForeground(new Color(255, 255, 255));
 		lblCurrentAmount.setBackground(new Color(255, 255, 255));
 		add(lblCurrentAmount, "cell 1 5");
@@ -213,7 +270,7 @@ public class StarDock extends JPanel  implements Serializable{
 		cashLabel.setForeground(new Color(255, 255, 255));
 		add(cashLabel, "flowx,cell 9 5");
 
-		JLabel lblCurrentHull = new JLabel("Current hull:");
+		JLabel lblCurrentHull = new JLabel("Current hull:"); // $codepro.audit.disable variableShouldBeFinal
 		lblCurrentHull.setForeground(new Color(255, 255, 255));
 		add(lblCurrentHull, "cell 1 6");
 
@@ -221,7 +278,7 @@ public class StarDock extends JPanel  implements Serializable{
 		currentHullLbl.setForeground(new Color(255, 255, 255));
 		add(currentHullLbl, "flowx,cell 9 6");
 
-		JLabel lblCurrentShip = new JLabel("Current Ship:");
+		JLabel lblCurrentShip = new JLabel("Current Ship:"); // $codepro.audit.disable variableShouldBeFinal
 		lblCurrentShip.setForeground(new Color(255, 255, 255));
 		add(lblCurrentShip, "cell 1 7");
 
@@ -229,7 +286,7 @@ public class StarDock extends JPanel  implements Serializable{
 		currShiplbl.setForeground(new Color(255, 255, 255));
 		add(currShiplbl, "flowx,cell 9 7,alignx left");
 
-		JLabel lblCurrentFuel = new JLabel("Current Fuel:");
+		JLabel lblCurrentFuel = new JLabel("Current Fuel:"); // $codepro.audit.disable variableShouldBeFinal
 		lblCurrentFuel.setForeground(new Color(255, 255, 255));
 		add(lblCurrentFuel, "cell 1 9");
 
@@ -239,32 +296,31 @@ public class StarDock extends JPanel  implements Serializable{
 
 		btnContinue = new JButton("Continue");
 		btnContinue.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) { // $codepro.audit.disable emptyMethod
 
 			}
 		});
 		add(btnContinue, "cell 9 10");
 
-		JList list = new JList();
+		JList list = new JList(); // $codepro.audit.disable variableShouldBeFinal
 		add(list, "cell 5 15");
 		updateLabels();
-		currFuelLbl.setText(decimalFormat(currPlayer.getShip().getCurrentFuel() - currPlayer.getShip().getFuelPerMove()) + "");
+		currFuelLbl.setText(decimalFormat(currPlayer.getShip().getCurrentFuel()
+				- currPlayer.getShip().getFuelPerMove()) + "");
 
-
-		JLabel label_1 = new JLabel("/");
+		JLabel label_1 = new JLabel("/"); // $codepro.audit.disable variableShouldBeFinal
 		label_1.setForeground(Color.WHITE);
 		add(label_1, "cell 9 6");
 
-		JLabel label = new JLabel("/");
+		JLabel label = new JLabel("/"); // $codepro.audit.disable variableShouldBeFinal
 		label.setForeground(Color.WHITE);
 		add(label, "cell 9 9");
-
 
 		maxHullLbl.setForeground(new Color(255, 255, 255));
 		add(maxHullLbl, "cell 9 6");
 
 		maxFuelLbl.setForeground(new Color(255, 255, 255));
-		add(maxFuelLbl, "cell 9 9");		
+		add(maxFuelLbl, "cell 9 9");
 
 	}
 
@@ -273,38 +329,42 @@ public class StarDock extends JPanel  implements Serializable{
 	 * @param longDouble amount passed in
 	 * @return new value #.## sentax
 	 */
-	public double decimalFormat(double longDouble)
-	{
+	public double decimalFormat(double longDouble){
 		DecimalFormat df = new DecimalFormat("#.##");
-		return Double.valueOf(df.format(longDouble));
+		return Double.valueOf(df.format(longDouble)); // $codepro.audit.disable handleNumericParsingErrors
 	}
 
 	/**
 	 * updates the labels on screen
 	 */
-	public void updateLabels()
-	{
+	public void updateLabels(){
 		currentHullLbl.setText(currPlayer.getShip().getHull() + "");
 		currShiplbl.setText(currPlayer.getShip().getName());
 		cashLabel.setText(currPlayer.getCash() + "");
-		currFuelLbl.setText(currPlayer.getShip().getCurrentFuel()+"");
-		maxHullLbl.setText(currPlayer.getShip().getMaxHull() +"");
-		maxFuelLbl.setText(currPlayer.getShip().getFuelCapacity() +"");
-		fuelSlider.setMaximum((int) (currPlayer.getShip().getFuelCapacity() - currPlayer.getShip().getCurrentFuel()));
-		hullSlider.setMaximum((int) (currPlayer.getShip().getMaxHull() - currPlayer.getShip().getHull()));
+		currFuelLbl.setText(currPlayer.getShip().getCurrentFuel() + "");
+		maxHullLbl.setText(currPlayer.getShip().getMaxHull() + "");
+		maxFuelLbl.setText(currPlayer.getShip().getFuelCapacity() + "");
+		fuelSlider.setMaximum((int) (currPlayer.getShip().getFuelCapacity() // $codepro.audit.disable lossOfPrecisionInCast
+				- currPlayer.getShip().getCurrentFuel()));
+		hullSlider.setMaximum((int) (currPlayer.getShip().getMaxHull() // $codepro.audit.disable lossOfPrecisionInCast
+				- currPlayer.getShip().getHull()));
 
 	}
 
 	/**
 	 * paint method for gui
+	 * @param g graphics 
 	 */
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		bg = new ImageIcon("img/bricks.jpg").getImage();
-		g.drawImage(bg, 0, 0,getWidth(),getHeight(), this);
+		g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
 	}
-	public void afterLoad()
-	{
+
+	/**
+	 * afterloading
+	 */
+	public void afterLoad(){
 		bg = new ImageIcon("img/bricks.jpg").getImage();
 	}
 
