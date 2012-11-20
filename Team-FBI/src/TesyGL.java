d7b63cbc06da0193b3c8071a308b0902dbf7cc91
sucrelv <sucrelv@b47cf0e7-3563-6f0a-96fe-3156760c1070>
2012-11-20 07:12:51 +0000

    JUnit test case added - Guang Lu

---
+++ Team-FBI/src/TesyGL.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test class to test the calculatorBuy() method
+ * in Trade class
+ * @author Guang Lu
+ */
+public class TesyGL {
+	/**
+	 * field trade
+	 */
+	private Trade trade;
+	
+	/**
+	 * field player
+	 */
+	private Player player;
+	
+	/**
+	 * field planet
+	 */
+	private Planet planet;
+	
+	/**
+	 * field techLevel
+	 */
+	private int techLevel;
+	
+	/**
+	 * set up fixture
+	 */
+	@Before 
+	public void setUp() { 
+        player=new Player("Guang");
+        planet=new Planet("test planet");
+        techLevel=planet.getTechLevel();
+        planet.setZone(new Zone(1));
+        trade=new Trade(player,planet);
+    }
+	
+	/**
+	 * test the method
+	 */
+	@Test
+	public void testCalculatorBuy() {
+		int result=trade.calculatorBuy(0);
+		int max = 30+(3*techLevel+30*4/100);
+		int min = 30-(3*techLevel+30*4/100);
+		boolean expected = (result<=max)&&(result>=min);
+	    assertTrue(expected);
+	}
+}
